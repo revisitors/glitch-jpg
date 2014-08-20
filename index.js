a7489f9a8e121c6a1ccd2ca1e21c85374c54ffd1
Ryan Kois <ryan.kois@gmail.com>
2014-08-19 21:04:32 -0700

    Fix a thing.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var fs = require('fs')
 
 module.exports = function(buf) {
   for (i = 400; i < buf.length - 1; i++) {
-    if (i % 3 === 2 && buf[i] > 1) {
-      buf[i] = rng(1,255)
+    if (buf[i] > 1) {
+      // over 254 and things do not work.
+      buf[i] = rng(1,254)
     }
   }
   return buf
